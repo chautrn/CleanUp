e66b7e410ecf06b01e6c72b3e2ee823b1f9e2bd7
brandonkhop <brandonkhop@gmail.com>
2020-10-31 16:19:35 -0400

    Started implementation of Django models
    - Started Django backend.
    - Added Player, Pet, and Friendship models.

---
+++ backend/api/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Pet(models.Model):
+    name = models.CharField(max_length=50)
+
+class Friendship(models.Model): 
+    creator = models.ForeignKey(Player, related_name='creator-relation')
+    friend = models.ForeignKey(Player, related_name='friend-relation')
+    
+class Player(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    pet = models.OneToOneField(Pet, on_delete=models.CASCADE)
+    is_display_name = models.BooleanField(default = False)
+    is_displaying_location = models.BooleanField(default = False)
+    is_donor = models.BooleanField(default= False)
+    experience = models.IntegerField(default=0)
+    

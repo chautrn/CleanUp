f17fcf45b343857bc836f38de5b407224565df64
chautrn <chautran.tai@gmail.com>
2020-11-05 02:17:37 -0500

    Used geolocation for map center

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import styled from 'styled-components'
 import L from 'leaflet';
 import { Map as MapContainer, TileLayer, ZoomControl } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
@@ -7,6 +6,7 @@ import mapTiles from './mapTiles';
 
 const Map = (props, ref) => {
 	const [mapTheme, setMapTheme] = useState(mapTiles.lightTheme)
+
 	useEffect(() => {
 		if (props.isDark) {
 			setMapTheme(mapTiles.darkTheme);
@@ -16,13 +16,15 @@ const Map = (props, ref) => {
 		}
 	}, [props.isDark]);
 
+	
+
 	return(
 		<div id='wrapper'>
 			<MapContainer 
 				style={{height: '100vh', width: '100vw'}} 
-				center={[37, -95]} 
+				center={props.geoPosition} 
 				zoomControl={false} 
-				zoom={4}
+				zoom={10}
 				minZoom={4}
 				maxBounds={[L.latLng(90, -Infinity), L.latLng(-80, Infinity)]}
 				maxBoundsViscosity={1.0}>
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import Map from '../components/Map';
-import styled from 'styled-components';
+import Control from 'react-leaflet-control';
 import { Button } from '@material-ui/core';
 
 const Home = () => {
 	const [isDark, setDark] = useState(false);
+	const [geoPosition, setGeoPosition] = useState([0, 0]); // [lat, long]
+
+	const geoSuccess = position => {
+		setGeoPosition([position.coords.latitude, position.coords.longitude]);
+	}
+
+	const geoError = () => {
+		console.log("internal geolocation error");
+	}
+
+	useEffect(() => {
+		if(!navigator.geolocation) {
+			console.log("Geolocator not enabled.");
+		}
+		else {
+			navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+		}
+	}, []);
 
 	useEffect(() => {
 		console.log(isDark);
@@ -12,10 +30,12 @@ const Home = () => {
 
 	return(
 		<div>
-			<Map isDark={isDark}>
-				<Button variant='contained' onClick={() => { setDark(!isDark) }} style={{zIndex: 1001}}>
-					Dark Mode
-				</Button>
+			<Map isDark={isDark} geoPosition={geoPosition}>
+				<Control position="topright">
+					<Button variant='contained' onClick={() => { setDark(!isDark) }}>
+						Dark Mode
+					</Button>
+				</Control>
 			</Map>
 		</div>
 	)
